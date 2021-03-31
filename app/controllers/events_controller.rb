11dd6fc5f75e4907c97a3ddd426c76111177c66b
gdumani <gdumani@gmail.com>
2021-03-31 03:21:26 -0600

    defined current user on applications controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,23 @@
 class EventsController < ApplicationController
-  
+  def index
+    @events=Event.all
+  end
+
   def show
-    @user = User.find_by(params[:user_id])
-    @event = Event.find_by(params[:id])
+    @user = User.find_by(id: session[:user_id])
+#    @event = User.Event.find_by(creator: @user.id])
+    @events = @user.events
   end
 
   def new 
-   @user = User.find_by(params[:user_id])
+   @user = User.find_by(id: session[:user_id])
    @event = @user.events.new
   end
 
 def create
-   @user = User.find_by(params[:user_id])
-   @event = @user.events.build(event_params)
+  #  @user = User.find_by(params[:user_id])
+  @user = User.find_by(id: session[:user_id])
+  @event = @user.events.build(event_params)
     if @event.save
         redirect_to events_show_path, notice: "Event was created successfully"
     else
