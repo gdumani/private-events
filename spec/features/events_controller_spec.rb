f5304bc92bbb9de6d7541ecd9eb9d61baa657fcd
gdumani <gdumani@gmail.com>
2021-04-07 16:48:36 -0600

    Deleted Event Spec comments

--- spec/features/events_controller_spec.rb
+++ spec/features/events_controller_spec.rb
@@ -1,58 +1,54 @@
-require 'rails_helper' 
+require 'rails_helper'
 
-RSpec.feature "EventsControllers", type: :feature do  
-  context 'Create new event' do 
+RSpec.feature 'EventsControllers', type: :feature do
+  context 'Create new event' do
     before :each do
       visit new_user_path
-      within('form') do 
+      within('form') do
         fill_in 'username',	with: 'Testname'
         fill_in 'Email', with: 'test@email'
       end
       click_button 'Sign Up'
       visit events_new_path
-    end 
-     scenario 'should be successful' do
-      within('form') do
-        fill_in 'Description', with: 'Event test'
-        select((Time.now.year+1), from: 'event_event_date_1i')
-      end
-      click_button 'Submit'
-      expect(page).to have_content('was created successfully')
-    end 
+    end
     scenario 'should be successful' do
       within('form') do
         fill_in 'Description', with: 'Event test'
-        select((Time.now.year-1), from: 'event_event_date_1i')
+        select((Time.now.year + 1), from: 'event_event_date_1i')
+      end
+      click_button 'Submit'
+      expect(page).to have_content('was created successfully')
+      expect(page).to have_content('Event test')
+    end
+    scenario 'should be successful' do
+      within('form') do
+        fill_in 'Description', with: 'Event test'
+        select((Time.now.year - 1), from: 'event_event_date_1i')
       end
       click_button 'Submit'
       expect(page).to have_content('Invalid date')
-    end 
-   end 
-   context 'Event index and user show pages' do 
+    end
+  end
+  context 'Event index and user show pages' do
     before :each do
       @user1 = User.create(username: 'u1', email: 'u1@mail')
-      # user2 = User.create(username: 'u2', email: 'u2@mail')
-      @event1 = @user1.events.create(description: 'User1 text', event_date: Time.now + 10000)
-      # event2 = user2.events.create(description: 'User2 text', event_date: Time.now + 10000)
-      # event2_old = user2.events.create(description: 'User2 old text', event_date: Time.now - 10000)
+      @event1 = @user1.events.create(description: 'User1 text', event_date: Time.now + 10_000)
       visit login_path
-      within('form') do 
+      within('form') do
         fill_in 'username',	with: 'u1'
       end
-      click_button 'Sign In' 
+      click_button 'Sign In'
     end
     scenario 'successful display of index page' do
       expect(page).to have_content('Upcoming Events')
-      expect(page).to have_content(@user1.events.first.description) 
+      expect(page).to have_content(@user1.events.first.description)
       expect(page).to have_content('Past Events')
     end
-
     scenario 'successful display of show page' do
       click_link('My Events')
       expect(page).to have_content('Created Events')
-      expect(page).to have_content(@user1.events.first.description) 
+      expect(page).to have_content(@user1.events.first.description)
       expect(page).to have_content('Attending Events')
     end
-
-   end
+  end
 end
