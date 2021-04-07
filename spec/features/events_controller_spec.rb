02cee20b7a8d8e8bea0e25dd2b3c52382ded609d
mina <takhminam@gmail.com>
2021-04-07 22:33:18 +0300

    Add scenario to the form

--- spec/features/events_controller_spec.rb
+++ spec/features/events_controller_spec.rb
@@ -4,13 +4,14 @@ RSpec.feature "EventsControllers", type: :feature do
   context 'Create new event' do 
     before :each do
       visit events_new_path
-    end
+    end 
+     scenario 'should be successful' do
       within('form') do
         fill_in 'Description', with: 'Event test'
-        select((Time.now+10000), :from => 'Event date')
+        select((Time.now+10000), from: 'Event date')
       end
       click_button 'Submit'
       expect(page).to have_content('was created successfuly')
-        
-  end
+    end 
+   end
 end
